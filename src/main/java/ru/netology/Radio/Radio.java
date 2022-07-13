d05a15d64e685df38fd8fbf8c531a21e08d32bc4
Екатерина Калистратова <kalistratova_k@mail.ru>
2022-07-14 00:11:11 +0300

    Radio Test

---
+++ src/main/java/ru/netology/Radio/Radio.java
@@ -0,0 +1,62 @@
+package ru.netology.Radio;
+
+public class Radio {
+    private int currentRadioStation;
+    private int currentVolume;
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
+        if (newCurrentRadioStation < 0) {
+            return;
+        }
+        if (newCurrentRadioStation > 9) {
+            return;
+        }
+        currentRadioStation = newCurrentRadioStation;
+    }
+    public void nextRadioStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation = currentRadioStation + 1;
+        }
+        if (currentRadioStation == 9) {
+            currentRadioStation = 0;
+        }
+    }
+    public void prevRadioStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation = currentRadioStation - 1;
+        }
+        if (currentRadioStation == 0) {
+            currentRadioStation = 9;
+        }
+    }
+    public int getCurrentVolume(){
+        return currentVolume;
+    }
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+    public void nextVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        if (currentVolume == 10) {
+            currentVolume = 0;
+        }
+    }
+    public void prevVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+        if (currentVolume == 0) {
+            currentVolume = 10;
+        }
+    }
+}
